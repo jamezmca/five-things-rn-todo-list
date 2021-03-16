8117d5d481737852fc1ccb82f894265f72c8c7ff
James McArthur <jamesmca@live.com>
2021-03-15 19:32:03 -0700

    app.js
    initialize component files and set up basic app screen structure

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/*use three different flex views to fit conent and adjust heights of each view */}
+      <Text>5 Things</Text>
+      <View style={styles.containers}></View>
+      <View style={styles.containerUno}><Text>Open up App.js to start working on your app!</Text></View>
+      <View style={styles.containerDos}><Text>Open up App.js to start working on your app!</Text></View>
+      <View style={styles.containerTres}><Text>Open up App.js to start working on your app!</Text></View>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+// color combo dark dark blue background with dark purple 
+//cards and light pink box shadow and white text
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containers: {
+
+  },
+  containerUno: {
+    flex: 3,
+    borderColor: 'black',
+    borderBottomWidth: 2
+  },
+  containerDos: {
+    flex: 2,
+  },
+  containerTres: {
+    flex: 1,
+  }
 });
---
+++ components/TodoInput.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TodoInput = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+};
+
+
+
+export default TodoInput;
